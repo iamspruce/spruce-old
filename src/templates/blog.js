401631cb6569dd2b894761ff34f493072b7d6f80
iamspruce <iamspruce8@gmail.com>
2021-05-06 14:07:59 +0100

    added two more posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -17,7 +17,7 @@ export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
+      <div className="wrapper">
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
