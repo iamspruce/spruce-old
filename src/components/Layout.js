034904ca85e25b6f5bd95ceb113502d2a1d99e8a
iamspruce <iamspruce8@gmail.com>
2021-06-09 13:41:59 +0100

    added a new image for seo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,15 +2,13 @@ import React from "react";
 import Header from "./Header";
 import "../css/main.css"
 import Footer from "./Footer";
-import { Helmet } from "react-helmet"
+import SEO from "./SEO";
 
 
 export default function Layout({ children }) {
   return (
     <div className="wrapper">
-      <Helmet htmlAttributes={{
-        lang: 'en'
-      }} />
+      <SEO />
       <Header />
       {children}
       <Footer />
