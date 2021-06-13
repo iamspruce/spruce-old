25d4817af31561f35b0c3654f58dec4fc2c08c11
iamspruce <iamspruce8@gmail.com>
2021-06-13 18:00:05 +0100

    added theme ui

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useState,useEffect } from "react"
 import theme from "../../content/theme.json"
 import Icon from "./Icon"
 
+const localFontSize = localStorage.getItem('font-size') || '15';
 
 const Theme = () => {
+  const [font, setFont] = useState(localFontSize);
+  const [scheme, setScheme] = useState('default'); 
+
+  
+  useEffect(() => {
+    localStorage.setItem('font-size', font)
+    let root = document.documentElement;
+    root.setAttribute('data-theme', `${scheme}`)
+    let fontSlider = document.querySelector('.theme-range__slider');
+    root.style.setProperty('--font-size', `${font}px`);
+    fontSlider.style.setProperty('--color-stop', `calc(((${font} / 20) * 100%) - 60%)`);
+  }, [font]);
+
+
   return (
     <div className="theme" aria-modal="true">
       <div className="theme-wrapper__inner wrapper">
         <div className="theme-header">
-        <strong className="theme-title text--large">Customize Theme</strong>
+        <strong className="theme-title textlarge">Customize Theme</strong>
           <button aria-label="close theme switcher" className="btn">
             <Icon name="icon-close" size="16" />
           </button>
@@ -21,7 +36,7 @@ const Theme = () => {
         <div className="theme-range">
               <label htmlFor="font">
             <span className="text-xsmall">Aa</span>
-              <input type="range" name="font" id="font-size" min="15" max="22" className="theme-range__slider" />
+              <input type="range" name="font" id="font-size" min="10" max="20" className="theme-range__slider" onChange={e => setFont(e.target.value)} value={font} />
                   <span className="text-large">Aa</span>
               </label>
         </div>
@@ -31,13 +46,13 @@ const Theme = () => {
         <div className="schemes">
           {theme.map((data) => {
             return (
-            <button className="scheme" aria-label={`${data.id}`} style={{ backgroundColor: `${data.colors["--bg"]}`, border: `1px solid ${data.colors["--color-primary"]}`}}>
+            <button onClick={e => setScheme(e.target.value)} className="scheme js-scheme" aria-label={`${data.id}`} value={`${data.id}`} style={{ backgroundColor: `${data.colors["bg"]}`, border: `1px solid ${data.colors["color-primary"]}`}}>
               <span className="scheme__pallete">
-              <span style={{ backgroundColor: `${data.colors["--color-primary"]}`}}></span>
-              <span style={{ backgroundColor: `${data.colors["--text"]}`}}></span>
-              <span style={{ backgroundColor: `${data.colors["--text-alt"]}`}}></span>
-              <span style={{ backgroundColor: `${data.colors["--border"]}`}}></span>
-              <span style={{ backgroundColor: `${data.colors["--bg-alt"]}`}}></span>
+              <span style={{ backgroundColor: `${data.colors["color-primary"]}`}}></span>
+              <span style={{ backgroundColor: `${data.colors["text"]}`}}></span>
+              <span style={{ backgroundColor: `${data.colors["text-alt"]}`}}></span>
+              <span style={{ backgroundColor: `${data.colors["border"]}`}}></span>
+              <span style={{ backgroundColor: `${data.colors["bg-alt"]}`}}></span>
               </span>
             </button>
             )
